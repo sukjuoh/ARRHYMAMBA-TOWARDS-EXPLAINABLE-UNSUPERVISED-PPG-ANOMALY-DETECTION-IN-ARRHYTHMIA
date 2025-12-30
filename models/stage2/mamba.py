99f740e3b3dde1d5e6eee907dc037f0e9414181a
sukjuoh <dhtjrwn119@gmail.com>
2025-12-30 19:28:43 +0900

    Add files via upload

---
+++ models/stage2/mamba.py
@@ -0,0 +1,171 @@
+
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from typing import Optional
+from functools import partial
+from einops import rearrange, repeat
+from models.stage2.ssm import mamba_inner_fn_no_out_proj
+from models.stage2.layer_norm import layer_norm_linear_fn, RMSNorm
+
+
+
+def drop_path(x, drop_prob: float = 0., training: bool = False, scale_by_keep: bool = True):
+    if drop_prob == 0. or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)
+    random_tensor = x.new_empty(shape).bernoulli_(keep_prob)
+    if keep_prob > 0.0 and scale_by_keep:
+        random_tensor.div_(keep_prob)
+    return x * random_tensor
+
+
+class DropPath(nn.Module):
+    def __init__(self, drop_prob: float = 0., scale_by_keep: bool = True):
+        super().__init__()
+        self.drop_prob = drop_prob
+        self.scale_by_keep = scale_by_keep
+
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training, self.scale_by_keep)
+
+
+def init_weights(module, n_layer, initializer_range=0.02, rescale_prenorm_residual=True, n_residuals_per_layer=1):
+    if isinstance(module, nn.Linear):
+        nn.init.xavier_normal_(module.weight)
+        if module.bias is not None and not getattr(module.bias, "_no_reinit", False):
+            nn.init.zeros_(module.bias)
+    elif isinstance(module, nn.Conv1d):
+        nn.init.kaiming_normal_(module.weight, mode='fan_in', nonlinearity='linear')
+        if module.bias is not None:
+            nn.init.zeros_(module.bias)
+    elif isinstance(module, nn.Embedding):
+        nn.init.normal_(module.weight, std=initializer_range)
+
+    if rescale_prenorm_residual:
+        for name, p in module.named_parameters():
+            if name in ["out_proj.weight", "fc2.weight"]:
+                with torch.no_grad():
+                    p /= math.sqrt(n_residuals_per_layer * n_layer)
+
+
+class BiMambaLayerV2(nn.Module):
+    """ Optimized Bi-directional Mamba Layer """
+    def __init__(self, dim, d_state=16, d_conv=3, dt_rank="auto", drop_path=0.1):
+        super().__init__()
+        self.dim = dim
+        self.d_state = d_state
+        self.d_conv = d_conv
+        self.dt_rank = math.ceil(dim / 16) if dt_rank == "auto" else dt_rank
+
+        self.in_proj = nn.Linear(dim, dim, bias = True)
+        self.conv_1d = nn.Conv1d(dim, dim // 2, kernel_size=d_conv, bias=True, padding="same", groups=dim // 2)
+        #self.conv_2d = nn.Conv2d(dim, dim // 2, kernel_size=(1, d_conv), bias=True, padding="same", groups=dim // 2)
+
+
+        A = repeat(torch.arange(1, self.d_state + 1, dtype=torch.float32), "n -> d n", d=dim // 2).contiguous()
+        #Ab = repeat(torch.arange(1, self.d_state + 1, dtype=torch.float32), "n -> d n", d=dim // 2).contiguous()
+        self.A_log = nn.Parameter(torch.log(A))
+        #self.A_log.no_weight_decay= True
+        
+        self.Ab_log = nn.Parameter(torch.log(A.clone()))
+        #self.Ab_log.no_weight_decay= True
+        
+        self.D = nn.Parameter(torch.ones(dim // 2))
+        #self.D.no_weight_decay = True
+        self.x_proj = nn.Linear(dim // 2, self.dt_rank + self.d_state * 2, bias=False)
+        self.dt_proj = nn.Linear(self.dt_rank, dim // 2, bias=True)
+
+        dt_init_std = self.dt_rank ** -0.5
+        nn.init.uniform_(self.dt_proj.weight, -dt_init_std, dt_init_std)
+        dt = torch.exp(torch.rand(dim // 2) * (math.log(0.1) - math.log(0.001)) + math.log(0.001)).clamp(min=1e-4)
+        self.dt_proj.bias = nn.Parameter(dt + torch.log(-torch.expm1(-dt)))
+        #self.dt_proj.bias._no_reinit = True
+        self.out_proj = nn.Linear(dim, dim, bias=True)
+
+    def forward(self, x):
+        batch, seqlen, dim = x.shape
+        #x = layer_norm_linear_fn(x, self.norm_weight, self.norm_bias, self.linear_weight, self.linear_bias)
+        #x_proj = rearrange(self.in_proj(x), "b l c -> b c l")
+        x_proj = self.in_proj(rearrange(x, 'b (f t) c -> (b f) t c', f=3))
+        #x_proj = rearrange(self.in_proj(x), "b l c -> b c l")
+        x_proj = rearrange(x_proj, "b l c -> b c l")
+        x_proj = self.conv_1d(x_proj)
+        
+        ## conv 2d
+        #x_proj = rearrange(x_proj, "(b h) c w -> b c h w", h=3)
+        #x_proj = self.conv_2d(x_proj)
+        #x_proj = rearrange(x_proj, "b c h w -> (b h) c w")       
+        
+        x_proj = F.silu(x_proj)
+        z_proj = x_proj.clone()
+        A = -torch.exp(self.A_log.float())
+        Ab = -torch.exp(self.Ab_log.float())
+
+        forward_out = mamba_inner_fn_no_out_proj(x_proj, self.x_proj.weight, self.dt_proj.weight, A, None, None, self.D.float(),
+                                                 delta_bias=self.dt_proj.bias.float(), delta_softplus=True)
+
+        backward_out = mamba_inner_fn_no_out_proj(x_proj.flip(dims=(-1,)), self.x_proj.weight, self.dt_proj.weight, Ab, None, None, self.D.float(),
+                                                  delta_bias=self.dt_proj.bias.float(), delta_softplus=True).flip(dims=(-1,))
+      
+        self._last_forward_out = forward_out.detach()
+        self._last_backward_out = backward_out.detach()
+
+        x_proj = forward_out + backward_out
+        #x_proj = forward_out
+
+        concat_out = torch.cat([x_proj, z_proj], dim=1)
+        concat_out = rearrange(concat_out, "b c l -> b l c")
+
+        return rearrange(self.out_proj(concat_out), '(b f) t c -> b (f t) c', f=3)
+
+
+class TransitionBlock(nn.Module):
+    def __init__(self, dim, expansion: int):
+        super().__init__()
+        self.proj_in = nn.Linear(dim, expansion * dim)
+        #self.depthwise_conv = nn.Conv1d(expansion * dim, expansion * dim, kernel_size=3, padding="same", groups=expansion * dim)
+        self.conv = nn.Conv2d(expansion * dim, expansion * dim, kernel_size=3, padding="same", groups=expansion * dim)
+        self.activation = nn.GELU()
+        self.proj_out = nn.Linear(expansion * dim, dim)
+
+    def forward(self, x):
+        #x = self.proj_in(x)
+        x = self.proj_in(rearrange(x, 'b (f t) c -> b f t c', f=3))
+        #b, l, c = x.size()
+        #x = rearrange(x, 'b l c -> b c l')
+        x = rearrange(x, 'b f t c -> b c f t')
+        x = self.conv(x)
+        #x = self.conv(x)
+        x = self.activation(x)
+        #x = rearrange(x, 'b c l -> b l c')     
+        x = rearrange(x, 'b c f t -> b f t c')       
+        return rearrange(self.proj_out(x), 'b f t c -> b (f t) c')
+
+
+class BiMambaBlockV2(nn.Module):
+    def __init__(self, dim, n_layer=1, drop_path=0.1):
+        super().__init__()
+        
+        self.mamba_layers = nn.ModuleList([
+            BiMambaLayerV2(dim, d_state=16, d_conv=3, drop_path=drop_path) for _ in range(n_layer)
+        ])
+        #self.conv = nn.Conv2d(dim, dim, kernel_size=3, padding="same", groups=dim)
+        self.norm = RMSNorm(dim)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. and self.training else nn.Identity()
+        self.mlp = TransitionBlock(dim, 4)
+
+        # 가중치 초기화
+        self.apply(lambda module: init_weights(module, n_layer=n_layer))
+
+    def forward(self, x):
+        #skip = x.clone()
+        for layer in self.mamba_layers:
+            x = x + self.drop_path(layer(x))
+        x = x + self.drop_path(self.mlp(self.norm(x)))
+        #x = self.mlp(self.norm(x))
+
+        return self.norm(x)
---
+++ models/stage2/transformer.py
@@ -0,0 +1,114 @@
+import torch
+from torch import nn, Tensor
+import torch.nn.functional as F
+from typing import Type
+from einops import rearrange
+from rotary_embedding_torch import RotaryEmbedding
+from models.stage2.layer_norm import RMSNorm
+
+
+
+class Attention(nn.Module):
+    def __init__(self, embedding_dim, num_heads, self_attn=True, rope_dim=None, dropout=0.0):
+        super().__init__()
+        self.embedding_dim = embedding_dim
+        self.num_heads = num_heads
+        self.self_attn = self_attn
+
+        assert embedding_dim % num_heads == 0
+        self.head_dim = embedding_dim // num_heads
+
+        rope_dim = rope_dim if rope_dim is not None else self.head_dim
+        #self.rotary_emb = RotaryEmbedding(dim=rope_dim)
+
+        self.q_proj = nn.Linear(embedding_dim, embedding_dim)
+        self.k_proj = nn.Linear(embedding_dim, embedding_dim)
+        self.v_proj = nn.Linear(embedding_dim, embedding_dim)
+        self.out_proj = nn.Linear(embedding_dim, embedding_dim)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def split_heads(self, x):
+        # x: (B, L, C)
+        b, l, c = x.shape
+        x = x.reshape(b, l, self.num_heads, self.head_dim)
+        return x.permute(0, 2, 1, 3)   # (B, H, L, D)
+
+    def merge_heads(self, x):
+        # x: (B, H, L, D)
+        b, h, l, d = x.shape
+        x = x.permute(0, 2, 1, 3).reshape(b, l, h * d)
+        return x
+
+    def forward(self, q: Tensor, k: Tensor = None, v: Tensor = None):
+        if self.self_attn:
+            k = v = q
+
+        # projections
+        q = self.q_proj(q)
+        k = self.k_proj(k)
+        v = self.v_proj(v)
+
+        # split heads → (B, H, L, D)
+        q = self.split_heads(q)
+        k = self.split_heads(k)
+        v = self.split_heads(v)
+
+        # RoPE
+        #q = self.rotary_emb.rotate_queries_or_keys(q)
+       # k = self.rotary_emb.rotate_queries_or_keys(k)
+
+        # scaled dot product attention
+        out = F.scaled_dot_product_attention(
+            q, k, v,
+            attn_mask=None,
+            dropout_p=self.dropout.p if self.training else 0.0,
+            is_causal=False,
+        )
+
+        # combine heads
+        out = self.merge_heads(out)
+
+        out = self.out_proj(out)     # (B,L,C)
+        out = self.dropout(out)
+        return out
+
+
+
+class SimpleTransformerBlock(nn.Module):
+    def __init__(self, embedding_dim, num_heads, H:int, dropout=0.0):
+        super().__init__()
+        self.H = H
+        self.self_attn = Attention(embedding_dim, num_heads, dropout=dropout)
+        self.conv = nn.Conv2d(embedding_dim, embedding_dim, kernel_size=3, padding='same', groups=embedding_dim)
+        self.drop = nn.Dropout(dropout)
+
+
+    def forward(self, x):
+        attn = self.self_attn(q=x)
+        attn = rearrange(attn, '(b h) w c -> b c h w', h=self.H)
+        attn = rearrange(self.conv(attn), 'b c h w -> (b h) w c')
+        x = x + self.drop(attn)
+
+        return x
+
+
+
+class SimpleTransformer(nn.Module):
+    def __init__(self, depth, embedding_dim, num_heads, H:int, dropout=0.0):
+        super().__init__()
+        self.linear1 = nn.Linear(embedding_dim, embedding_dim*4)
+        self.layers = nn.ModuleList([
+            SimpleTransformerBlock(embedding_dim*4, num_heads, H, dropout=dropout)
+            for _ in range(depth)
+        ])
+        self.norm = RMSNorm(embedding_dim*4, eps=1e-5)
+        self.linear2 = nn.Linear(embedding_dim*4, embedding_dim)
+    
+    def forward(self, x):
+        x = self.linear1(x)
+        for layer in self.layers:
+            x = layer(x)
+        x = self.norm(x) 
+        return self.linear2(x)
+
