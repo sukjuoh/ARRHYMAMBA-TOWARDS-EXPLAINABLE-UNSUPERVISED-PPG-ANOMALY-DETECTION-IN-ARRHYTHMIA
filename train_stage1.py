64f36588880439f4334c2f4ed1384f5a4f77109e
sukjuoh <dhtjrwn119@gmail.com>
2025-12-31 15:07:11 +0900

    Update train_stage1.py

--- train_stage1.py
+++ train_stage1.py
@@ -16,9 +16,9 @@ from pytorch_lightning.callbacks import EarlyStopping
 
 from exp.set_stage1 import SetStage1
 
-from preprocessing.preprocess import PPG_NormalSequence
-from preprocessing.data_pipeline import build_pretrain_data_pipeline
-from utils import get_root_dir
+from preprocessing.load_data import PPG_NormalSequence
+from preprocessing.serve_data import build_pretrain_data_pipeline
+from utils import get_root_dir, load_yaml_param_settings
 
 
 def load_args():
@@ -26,7 +26,6 @@ def load_args():
     parser.add_argument('--config', type=str, help="Path to the config data file.",
                         default=get_root_dir().joinpath('configs', 'config.yaml'))
     parser.add_argument('--gpu_device_ind', nargs='+', default=[0], type=int)
-    parser.add_argument('--input_shape', default=200, type=int)
     return parser.parse_args()
 
 
@@ -80,3 +79,34 @@ def train_stage1(config: dict,
     )
     
     return trainer
+if __name__ == '__main__':
+    args = load_args()
+    config = load_yaml_param_settings(args.config)
+
+    data_path = Path(config['dataset']['train_path'])
+    batch_size = config['dataset']['batch_sizes']['stage1']
+    num_workers = config['dataset']["num_workers"]
+
+    dataset_importer = PPG_NormalSequence.load_data(data_path)
+    train_data_loader, test_data_loader = [
+            build_pretrain_data_pipeline(
+                batch_size=batch_size,
+                data=dataset_importer.data,
+                kind=kind,
+                config = config,
+                num_workers=num_workers
+            )
+            for kind in ['train', 'test']
+        ]
+    
+    trainer = train_stage1(
+                config=config,
+                train_data_loader=train_data_loader,
+                test_data_loader=test_data_loader,
+                gpu_device_ind=args.gpu_device_ind,
+                checkpoint_path=None, 
+            )
+
+    checkpoint_path = 'saved_models/stage1.ckpt'
+    trainer.save_checkpoint(checkpoint_path)
+    wandb.finish()
